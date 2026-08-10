276668ea2f68f09b77f96a9c64c1462ddd7bc0de
TheCryptoArborist <ptshopping63@gmail.com>
2026-08-10 15:32:16 -0500

    Add Toomeys Mardi Gras to gear resources

--- src/lib/seed-data.ts
+++ src/lib/seed-data.ts
@@ -238,6 +238,7 @@ export const resourceSeeds: ResourceSeed[] = [
   directoryResource("Port City Throws", "Mardi Gras Gear / Throws", "Mardi Gras throws and gear resource.", 1),
   directoryResource("Pop's Midtown", "Mardi Gras Gear / Throws", "Mardi Gras gear and shopping resource.", 2),
   directoryResource("Lemon T's", "Mardi Gras Gear / Throws", "Mardi Gras shirts, apparel, and gear resource.", 3),
+  directoryResource("Toomey's Mardi Gras", "Mardi Gras Gear / Throws", "Mardi Gras gear, throws, costumes, and party supplies resource.", 4, "https://toomeysmardigras.com/"),
   directoryResource("Mardi Gras 2026 Playlist", "Previous Parade Seasons", "Previous parade season playlist from the Mobile Mardi Gras video archive.", 1, MARDI_GRAS_2026_PLAYLIST_URL),
   directoryResource("Mardi Gras 2025 Playlist", "Previous Parade Seasons", "Previous parade season playlist from the Mobile Mardi Gras video archive.", 2, MARDI_GRAS_2025_PLAYLIST_URL),
   directoryResource("Mardi Gras 2024 Playlist", "Previous Parade Seasons", "Previous parade season playlist from the Mobile Mardi Gras video archive.", 3, MARDI_GRAS_2024_PLAYLIST_URL),
